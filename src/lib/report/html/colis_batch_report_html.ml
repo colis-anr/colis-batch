e97188dc0d47b40afe9a2c7edbc9ccd061c16ee5
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2019-11-12 16:16:15 +0100

    html report in its own submodule

--- src/lib/report/html/colis_batch_report_html.ml
+++ src/lib/report/html/colis_batch_report_html.ml
@@ -1,17 +1,5 @@
 open Colis_batch_ext
 
-let lwt_unix_write_whole_string fd str =
-  let len = String.length str in
-  let rec aux pos =
-    Lwt.bind (Lwt_unix.write_string fd str pos (len - pos))
-    @@ function
-    | 0 -> failwith "lwt_unix_write_whole_string"
-    | wrote ->
-      let pos = pos + wrote in
-      if pos < len then aux pos else Lwt.return_unit
-  in
-  aux 0
-
 let extract_static ~prefix =
   [
     "highlight.js/9.14.2/styles/github.min.css", [%blob "static/highlight.js/9.14.2/styles/github.min.css"];
