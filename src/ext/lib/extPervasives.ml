daf783aef3f001e7c733ffe0761812a56d178209
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2019-11-25 18:18:17 +0100

    more pervasives; including `significant`

--- src/ext/lib/extPervasives.ml
+++ src/ext/lib/extPervasives.ml
@@ -9,3 +9,11 @@ let spf = Format.sprintf
 let aspf = Format.asprintf
 
 let soi = string_of_int
+let iof = int_of_float
+let foi = float_of_int
+
+let fnot f x = not (f x)
+
+let significant ~precision n =
+  let l = 10. ** ((foi precision) -. 1. -. floor (log10 n)) in
+  (floor ((n *. l) +. 0.5)) /. l
