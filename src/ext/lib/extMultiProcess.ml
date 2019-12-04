7201b3efaba9a5da589c8bfe3a569c0614b75849
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2019-12-04 16:08:26 +0100

    mapi_p & iteri_p

--- src/ext/lib/extMultiProcess.ml
+++ src/ext/lib/extMultiProcess.ml
@@ -15,28 +15,33 @@ let forkee inputs_ichan outputs_ochan f =
   (
     try
       while true do
-        let input = input_value inputs_ichan in
-        let output = try Ok (f input) with exn -> Error exn in
+        let (index, input) = input_value inputs_ichan in
+        let output = try Ok (f index input) with exn -> Error exn in
         output_value outputs_ochan output;
         flush outputs_ochan;
       done
     with
-      _exn -> ()
+      exn ->
+      Format.eprintf
+        "Unexpected exception in forkee:@\n%s@\n%s@."
+        (Printexc.to_string exn)
+        (Printexc.get_backtrace ());
+      raise exn
   );
   exit 0
 
 let forker inputs_ochan outputs_ichan q a =
   try%lwt
     while%lwt true do
-      let (input, output_index) = Queue.take q in
-      let%lwt () = Lwt_io.write_value inputs_ochan input in
+      let (index, input) = Queue.take q in
+      let%lwt () = Lwt_io.write_value inputs_ochan (index, input) in
       let%lwt () = Lwt_io.flush inputs_ochan in
       let%lwt output = Lwt_io.read_value outputs_ichan in
-      if a.(output_index) <> None then
+      if a.(index) <> None then
         raise Error;
       match output with
       | Ok output ->
-        a.(output_index) <- Some output;
+        a.(index) <- Some output;
         Lwt.return ()
       | Error exn ->
         Lwt.fail exn
@@ -44,7 +49,7 @@ let forker inputs_ochan outputs_ichan q a =
   with
   | Queue.Empty -> Lwt.return ()
 
-let map_p ~workers f l =
+let mapi_p ~workers f l =
   let rec create_workers q a acc wid =
     if wid >= workers then
       acc
@@ -85,7 +90,7 @@ let map_p ~workers f l =
   let (_ichan, _ochan) = Lwt_io.pipe () in
   let q = Queue.create () in
   let a = Array.make (List.length l) None in
-  List.iteri (fun i x -> Queue.add (x, i) q) l;
+  List.iteri (fun i x -> Queue.add (i, x) q) l;
   let%lwt () = create_workers q a [] 0 |> List.rev |> lwt_join_or_first_error in
   Array.fold_right
     (fun b l ->
@@ -96,6 +101,12 @@ let map_p ~workers f l =
     []
   |> Lwt.return
 
-let iter_p ~workers f l =
-  let%lwt _ = map_p ~workers f l in
+let iteri_p ~workers f l =
+  let%lwt _ = mapi_p ~workers f l in
   Lwt.return_unit
+
+let map_p ~workers f l =
+  mapi_p ~workers (fun _ -> f) l
+
+let iter_p ~workers f l =
+  iteri_p ~workers (fun _ -> f) l
--- src/ext/lib/extMultiProcess.mli
+++ src/ext/lib/extMultiProcess.mli
@@ -1,3 +1,5 @@
 val map_p : workers:int -> ('a -> 'b) -> 'a list -> 'b list Lwt.t
+val mapi_p : workers:int -> (int -> 'a -> 'b) -> 'a list -> 'b list Lwt.t
 
 val iter_p : workers:int -> ('a -> unit) -> 'a list -> unit Lwt.t
+val iteri_p : workers:int -> (int -> 'a -> unit) -> 'a list -> unit Lwt.t
